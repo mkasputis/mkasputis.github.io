241070eff17587dd9f6f335f9a4c0294b97b22b0
Matthew Kasputis <mkasputis@gmail.com>
2021-07-20 08:25:14 -0400

    adding typescript

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Inside the source code you'll find the React w/ Redux App powering the github pa
 
 ### TODOs
 
-* Add Delete Buttons for drawn shapes
-* Add Download Buttons for GeoJSON of shapes
-* Make location persistent with refreshes
-* Find out why shape borders come out darker after converting from leaflet layer to GeoJSON back to layer
+- Add Delete Buttons for drawn shapes
+- Add Download Buttons for GeoJSON of shapes
+- Make location persistent with refreshes
+- Find out why shape borders come out darker after converting from leaflet layer to GeoJSON back to layer
+- Fix deployment warning 467 KiB main.js (webpack recommends 244 KiB and suggests https://webpack.js.org/guides/code-splitting/)
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,35 @@
-const path = require('path');
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 module.exports = {
-    entry: './src/index.jsx',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-              test: /\.css$/,
-              use: ['style-loader', 'css-loader'],
-            },
-            {
-              test: /\.svg$/,
-              use: ['xml-loader'],
-            },
-            {
-              test: /\.(png|woff|woff2)$/,
-              use: ['url-loader'],
-            },
-        ]
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
+  entry: "./src/index.jsx",
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["xml-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2)$/,
+        use: ["url-loader"],
+      },
     ],
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    devServer: {
-        contentBase: './dist',
-        port: 8080,
-    },
-    devtool: 'cheap-module-source-map',
-}
+  },
+  //plugins: [new webpack.HotModuleReplacementPlugin()],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  devServer: {
+    contentBase: "./dist",
+    port: 8080,
+  },
+  devtool: "source-map",
+};
